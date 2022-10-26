82678562d49d322c581314b56f37ee02d05a69d5
alighaffar123 <alighaffar.mangocoders@gmail.com>
2022-10-26 14:24:17 +0500

    new test data

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: [
+
+    './index.html',
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  }
+  // ...
+}
